c3279a90a3f8b234dc64ec42983fef447384aac2
Ali Villegas <alibryan.villegas@gmail.com>
2020-11-18 15:16:02 -0600

    added calendar login

--- client/src/Components/Widgets/FavClassData.js
+++ client/src/Components/Widgets/FavClassData.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { Box, Heading, SimpleGrid } from '@chakra-ui/core';
+import { Box, Heading, SimpleGrid, Button } from '@chakra-ui/core';
 
 
 const FavClassData = ({user, classR, handleChange}) => {
+    var gapi = window.gapi
+        var CLIENT_ID = "598537861437-v3cuakhpdh6jdv5bvo0br3j7r0m2omnt.apps.googleusercontent.com"
+        var API_KEY = "0sfHjbMNwHfXxRaP8O9Sr_J3"
+        var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+        var SCOPES = "https://www.googleapis.com/auth/calendar.events"
+    const handleCalendar = () => {
+        gapi.load('client:auth2', () => {
+            console.log('loaded client') })
+            gapi.client.init({
+                apiKey: API_KEY,
+                clientId: CLIENT_ID,
+                discoveryDocs: DISCOVERY_DOCS,
+                scope: SCOPES,
+            })
+            gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+            gapi.auth2.getAuthInstance().signIn()
+            .then(() => {})
+    }
+
     return (
         <SimpleGrid columns={3} border="1px" borderRadius="md" borderColor="gray.600" textAlign="center">
             <Box>
@@ -10,6 +29,9 @@ const FavClassData = ({user, classR, handleChange}) => {
             </Box>
             <Box>{classR.name}</Box>
             <Box>{classR.building}</Box>
+            <Button size="xs" variantColor="blue" onClick={handleCalendar}>
+                            Add to Google
+            </Button>
         </SimpleGrid>
     );
 }
