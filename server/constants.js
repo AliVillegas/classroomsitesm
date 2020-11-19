57a0264a7f6d51054978ac4b1a539a61d6e2108d
Ali Villegas <alibryan.villegas@gmail.com>
2020-11-18 21:12:19 -0600

    commit to aws branch

--- server/constants.js
+++
@@ -1,6 +0,0 @@
- exports.campusAdmin = "admin"
- exports.student = "student"
- exports.professor = "professor"
- exports.superAdmin = "superAdmin"
- exports.depAdmin = "adminDep"
- exports.queryLimit = 10000
